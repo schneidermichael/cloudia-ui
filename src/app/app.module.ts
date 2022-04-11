ac81c8ca8259463a261da989d3a447d0690b1c22
schneidermichael <schneidermichael@outlook.com>
2022-04-11 19:43:19 +0200

    fix: some bugs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,41 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component';
-import { NavbarComponent } from './navbar/navbar.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { LostPasswordComponent } from './lost-password/lost-password.component';
-<<<<<<< Updated upstream
-=======
 import { AuthorizedModule } from './authorized/authorized.module';
 import { UnauthorizedModule } from './unauthorized/unauthorized.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
->>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent,
-    NavbarComponent,
     LoginComponent,
     RegisterComponent,
     LostPasswordComponent
   ],
   imports: [
     BrowserModule,
-<<<<<<< Updated upstream
-    AppRoutingModule
-=======
     AppRoutingModule,
     AuthorizedModule,
     UnauthorizedModule,
     ReactiveFormsModule,
     HttpClientModule
 
->>>>>>> Stashed changes
   ],
   providers: [],
   bootstrap: [AppComponent]
