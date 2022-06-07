2aacb69084001f8b431175c8e84bf80f5c541ede
schneidermichael <schneidermichael@outlook.com>
2022-06-07 21:57:36 +0200

    fix: error message in tests

--- src/app/authorized/profile/profile.component.spec.ts
+++ src/app/authorized/profile/profile.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ProfileComponent } from './profile.component';
 import {HttpClientTestingModule} from "@angular/common/http/testing";
 import {RouterTestingModule} from "@angular/router/testing";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 describe('ProfileComponent', () => {
   let component: ProfileComponent;
@@ -10,7 +11,7 @@ describe('ProfileComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports : [ HttpClientTestingModule, RouterTestingModule],
+      imports : [ HttpClientTestingModule, RouterTestingModule, ReactiveFormsModule, FormsModule],
       declarations: [ ProfileComponent ]
     })
     .compileComponents();
