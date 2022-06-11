a2c41539461dc18bea4fd833181c94c3d6790218
schneidermichael <schneidermichael@outlook.com>
2022-06-11 18:34:34 +0200

    fix: deprecated dependency + test error

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,21 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import {TestBed} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {AppComponent} from './app.component';
+import {NavbarComponent} from "./unauthorized/navbar/navbar.component";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {FooterComponent} from "./unauthorized/footer/footer.component";
 
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule, HttpClientTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        NavbarComponent,
+        FooterComponent
       ],
     }).compileComponents();
   });
