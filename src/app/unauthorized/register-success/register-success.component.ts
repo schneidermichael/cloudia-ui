eb341bdd081924ec3208b56ca895cbff6cd2cb4c
Karim Schierbauer <karim.schierbauer@outlook.com>
2022-06-12 18:00:55 +0200

    feat: added confirmation to UI
    added confirmation routing

--- src/app/unauthorized/register-success/register-success.component.ts
+++ src/app/unauthorized/register-success/register-success.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Confirmation } from 'src/app/shared/model/confirmation.model';
+import { AuthenticationService } from 'src/app/shared/service/authentication.service';
 
 @Component({
   selector: 'app-register-success',
   templateUrl: './register-success.component.html'
 })
-export class RegisterSuccessComponent {
+export class RegisterSuccessComponent implements OnInit {
 
-  constructor() { /* TODO document why this constructor is empty */  }
+  token: string | undefined | null;
+  returnString: Confirmation | undefined | null;
 
 
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private authenticationService: AuthenticationService
+    ) { /* TODO document why this constructor is empty */  }
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.token = params['token'];
+      console.error(this.token);
+      if (this.token != undefined) {
+        this.authenticationService.confirm(this.token)
+          .subscribe(confirmation => {
+            console.error(confirmation.status);
+            if (confirmation.status == "isActivated") {
+              this.router.navigateByUrl('login')
+            }
+          })
+
+      }
+    })
+  }
 }
