12d407281d506ef8e8e0421040e91e9ba2df4283
schneidermichael <schneidermichael@outlook.com>
2022-07-12 13:46:08 +0200

    fix: docker compose deployment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:3000/v1',
+  apiBaseUrl: 'http://localhost:3000',
 
   //Variables for tests
   password: 'hallo123',
