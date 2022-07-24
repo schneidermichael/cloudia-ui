b7b4e8030ee4aeac60baf7a02c4980a0fb279132
schneidermichael <schneidermichael@outlook.com>
2022-07-24 18:18:25 +0200

    BREAKING CHANGE: changed port to 4000

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:3000',
+  apiBaseUrl: 'http://localhost:4000',
 
   //Variables for tests
   password: 'hallo123',
