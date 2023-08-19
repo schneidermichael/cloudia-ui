0f78ba7a7760cdaead294ea5361126c0081b663c
schneidermichael <schneidermichael@outlook.com>
2023-08-19 14:58:37 +0200

    chore: updated angular

--- src/app/shared/guard/authentication.guard.ts
+++ src/app/shared/guard/authentication.guard.ts
@@ -1,15 +1,12 @@
 import {Injectable} from '@angular/core';
-import {
-  CanActivate,
-  Router,
-} from '@angular/router';
+import { Router } from '@angular/router';
 import {Observable} from 'rxjs';
 import {AuthenticationService} from "../service/authentication.service";
 
 @Injectable({
   providedIn: 'root',
 })
-export class AuthenticationGuard implements CanActivate {
+export class AuthenticationGuard  {
   constructor(private authService: AuthenticationService, private router: Router) {
   }
 
